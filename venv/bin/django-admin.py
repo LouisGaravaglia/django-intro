55dd682fb7ab4ccea18b6e4b769cd637ccd9d89a
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-03-15 14:25:25 -0700

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/louieg3/Documents/Web_development/django-intro/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
